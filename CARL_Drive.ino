ca898621da349843b54b6f4c68d54e90b0bd707f
Scott Ferguson <esfergus@gmail.com>
2017-02-17 09:16:42 -0600

    Added inputs for LED mapped to headlights and lift/lower wedge mapped to back lights
    TODO includes implementing the lift/lower wedge methods.

--- CARL_Drive.ino
+++ CARL_Drive.ino
@@ -12,6 +12,10 @@
 
 Servo myservo1;
 Servo myservo2;
+Servo wedgeServo;
+
+string diodeColor = "white";
+int wedgePosition = 0;
 
 //Sets up pins 4 and 5 to be used for serial communication (optional, not used in default configuration)
 SoftwareSerial softSerial(5, 4); // RX, TX
@@ -22,13 +26,16 @@ void setup() {
   //Setup serial connections for Monitor and Bluetooth
   softSerial.begin(9600);
   Serial.begin(9600);
+  Serial.println("Serial online!");
 
   //Connects to the servos
   pinMode (2, OUTPUT);
   pinMode (3, OUTPUT);
+  pinMode (4, OUTPUT);
 
   myservo1.attach(2);
   myservo2.attach(3);
+  wedgeServo.attach(4);
 
   Serial.println("Setup Complete!");
 }
@@ -57,6 +64,18 @@ void robotStop (int duration) {
   myservo2.write(90);
 }
 
+void liftWedge () {
+  // TODO
+  wedgePosition = 1;
+  Serial.println("Lift wedge");
+}
+
+void lowerWedge () {
+  // TODO
+  wedgePosition = 0;
+  Serial.println("Lower wedge");
+}
+
 void loop() {
   if (Serial.available())
   { //if there is data being recieved
@@ -80,6 +99,31 @@ void loop() {
     if (input == 'S' )
     {
       robotStop(1);
+    }
+    if (input == 'W' || input == 'w') // Maps to "front lights on/off" in app
+    {
+      switch (diodeColor) {
+        case "white":
+          setColor(255, 0, 0);
+          diodeColor = "red";
+          break;
+        case "red":
+          setColor(0, 255, 0);
+          diodeColor = "green";
+          break;
+        case "green":
+          setColor(0, 0, 255);
+          diodeColor = "blue";
+          break;
+        case "blue":
+          setColor(255, 255, 255);
+          diodeColor = "white";
+          break;
+      }
+    }
+    if (input == 'U' || input == 'u') // Maps to "back lights on/off" in app
+    {
+      (wedgePosition == 0) ? liftWedge() : lowerWedge();
     }
   }
 }
