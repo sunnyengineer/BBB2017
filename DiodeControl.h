56680c5431e9644cecf40c4e1fa35c83c3ebe59f
Scott Ferguson <esfergus@gmail.com>
2017-02-18 09:28:28 -0600

    removed LED loop

--- DiodeControl.h
+++ DiodeControl.h
@@ -15,23 +15,7 @@ void setup()
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);  
 }
- 
-void loop()
-{
-  setColor(255, 0, 0);  // red
-  delay(1000);
-  setColor(0, 255, 0);  // green
-  delay(1000);
-  setColor(0, 0, 255);  // blue
-  delay(1000);
-  setColor(255, 255, 0);  // yellow
-  delay(1000);  
-  setColor(80, 0, 80);  // purple
-  delay(1000);
-  setColor(0, 255, 255);  // aqua
-  delay(1000);
-}
- 
+
 void setColor(int red, int green, int blue)
 {
   #ifdef COMMON_ANODE
