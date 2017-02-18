2ca42fafdc02babe5fbffdd2263c352ec612c73d
Sunny Harshdeep <sunnyengineer@users.noreply.github.com>
2017-02-18 17:08:55 -0600

    Update for competition

--- CARL_Drive.ino
+++ CARL_Drive.ino
@@ -8,13 +8,19 @@
 
 #include <Servo.h>
 #include <SoftwareSerial.h>
-#include <DiodeControl.h>
+ 
+int redPin = 5;
+int greenPin = 6;
+int bluePin = 7;
+ 
+//uncomment this line if using a Common Anode LED
+#define COMMON_ANODE
 
 Servo myservo1;
 Servo myservo2;
-Servo wedgeServo;
+Servo spikeServo;
 
-string diodeColor = "white";
+int diodeColor = 0;
 int wedgePosition = 0;
 
 //Sets up pins 4 and 5 to be used for serial communication (optional, not used in default configuration)
@@ -23,6 +29,7 @@ SoftwareSerial softSerial(5, 4); // RX, TX
 char input = 'S';
 
 void setup() {
+  
   //Setup serial connections for Monitor and Bluetooth
   softSerial.begin(9600);
   Serial.begin(9600);
@@ -32,48 +39,82 @@ void setup() {
   pinMode (2, OUTPUT);
   pinMode (3, OUTPUT);
   pinMode (4, OUTPUT);
+  // Connects the LEDs
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);  
 
   myservo1.attach(2);
   myservo2.attach(3);
-  wedgeServo.attach(4);
+  spikeServo.attach(4);
 
   Serial.println("Setup Complete!");
 }
+/*void setHighLow(int color) {
+  if(color == 0){
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(bluePin, HIGH);
+  }
+  else if(color == 1){
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, LOW);
+  }
+  else if(color == 2){
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(bluePin, HIGH);
+  }
+  else if(color == 3){
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(bluePin, HIGH);
+  }
+}*/
+void setColor(int red, int green, int blue){
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  digitalWrite(redPin, red);
+  digitalWrite(greenPin, green);
+  digitalWrite(bluePin, blue);  
+}
 
 void robotForward (int duration) {
-  myservo1.write(45);
-  myservo2.write(135);
+  myservo2.write(45);
+  myservo1.write(135);
 }
 
 void robotBackward (int duration) {
-  myservo1.write(135);
-  myservo2.write(45);
+  myservo2.write(135);
+  myservo1.write(45);
 }
 
 void robotRight (int duration) {
-  myservo1.write(135);
   myservo2.write(135);
+  myservo1.write(135);
 }
 
 void robotLeft (int duration) {
-  myservo1.write(45);
   myservo2.write(45);
+  myservo1.write(45);
 }
 void robotStop (int duration) {
   myservo1.write(90);
   myservo2.write(90);
 }
 
-void liftWedge () {
-  // TODO
-  wedgePosition = 1;
-  Serial.println("Lift wedge");
+void spikeDown (int duration) {
+  spikeServo.write(135);
+  Serial.println("Spike Down");
 }
 
-void lowerWedge () {
-  // TODO
-  wedgePosition = 0;
-  Serial.println("Lower wedge");
+void spikeUp (int duration) {
+  spikeServo.write(45);
+  Serial.println("Spike Up");
 }
 
 void loop() {
@@ -102,30 +143,35 @@ void loop() {
     }
     if (input == 'W' || input == 'w') // Maps to "front lights on/off" in app
     {
-      switch (diodeColor) {
-        case "white":
+       
+        if(diodeColor == 0) { // white
           setColor(255, 0, 0);
-          diodeColor = "red";
-          break;
-        case "red":
+          //setHighLow(1)
+          diodeColor = 1;
+        } else if (diodeColor == 1) { // red
           setColor(0, 255, 0);
-          diodeColor = "green";
-          break;
-        case "green":
+          //setHighLow(2);
+          diodeColor = 2;
+        } else if (diodeColor == 2) { // green
           setColor(0, 0, 255);
-          diodeColor = "blue";
-          break;
-        case "blue":
+          //setHighLow(3);
+          diodeColor = 3;
+        } else if (diodeColor == 3) { // blue
           setColor(255, 255, 255);
-          diodeColor = "white";
-          break;
-      }
+          //setHighLow(0);
+          diodeColor = 0;
+        }
     }
+    
     if (input == 'U' || input == 'u') // Maps to "back lights on/off" in app
     {
-      (wedgePosition == 0) ? liftWedge() : lowerWedge();
+      spikeDown(1);
+    }
+
+    if (input == 'V' || input == 'v') // Maps to "back lights on/off" in app
+    {
+      spikeUp(1);
     }
   }
 }
-
 
